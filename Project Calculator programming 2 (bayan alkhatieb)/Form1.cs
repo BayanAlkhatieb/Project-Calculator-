0962a977219d18b1ebf4d52f0750b4b3f4078bbe
BayanAlkhatieb <bayangold3@gmail.com>
2024-05-11 14:22:45 +0300

    Add files via upload

---
+++ Project Calculator programming 2 (bayan alkhatieb)/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Calculator_programming_2__bayan_alkhatieb_
+{
+   
+
+    public partial class Form1 : Form
+    {      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Button bn = (Button)sender;
+
+            if (textBox1.Text == "0" || isoperat == true)
+            { textBox1.Clear(); }
+
+
+            if (bn.Text == ".")
+            {
+                if(!textBox1.Text.Contains("."))
+                { textBox1.Text = textBox1.Text + bn.Text; }
+            }
+            else
+            {
+                textBox1.Text = textBox1.Text + bn.Text;
+            }
+           isoperat = false;
+        }
+
+
+
+        string operation = "";
+        double res = 0;
+        bool isoperat = false;
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            Button bn = (Button)sender;
+
+            operation = bn.Text;
+
+            res = Convert.ToDouble(textBox1.Text);
+
+            isoperat = true;
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            switch(operation)
+            {
+                case "+":
+                    textBox1.Text = (res + Convert.ToDouble(textBox1.Text)).ToString();
+                    break;
+
+                case "-":
+
+                    textBox1.Text = (res - Convert.ToDouble(textBox1.Text)).ToString();
+                    break;
+
+
+                case "*":
+                    textBox1.Text = (res * Convert.ToDouble(textBox1.Text)).ToString();
+                    break;
+
+
+                case "/":
+                    textBox1.Text = (res / Convert.ToDouble(textBox1.Text)).ToString();
+                    break;
+            }
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+
+            res = 0;
+        }
+    }
+}
---
+++ Project Calculator programming 2 (bayan alkhatieb)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_Calculator_programming_2__bayan_alkhatieb_
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
